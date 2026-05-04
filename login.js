73e0297d3ce80498b14748622394bd2912169c08
Mohamad Zalzale <mhmdzalzaleh16@gmail.com>
2026-05-04 17:28:10 -0400

    added signup

--- login.js
+++ login.js
@@ -1,6 +1,6 @@
 const userName = document.querySelector("#userName");
 const userPass = document.querySelector("#userPass");
-const loginBtn =  document.querySelector("#loginBtn");
+const loginBtn = document.querySelector("#loginBtn");
 
 loginBtn.addEventListener("click", function(){
     console.log(userName.value);
