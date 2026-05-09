328ad87bfbc23a09eb3d61daa10c3f85c27f297a
Mohamad Zalzale <mhmdzalzaleh16@gmail.com>
2026-05-09 00:14:22 -0400

    added dashboard logic

---
+++ dashboard.js
@@ -0,0 +1,13 @@
+const welcomeTxt = document.getElementById("welcomeTxt");
+const currentUser = localStorage.getItem("currentUser");
+console.log("current user ", currentUser);
+if(!currentUser) {
+    window.location.href = "login.html";
+} else {
+    welcomeTxt.textContent = `Welcome, ${currentUser}!`;
+}
+const dashboardBtn = document.getElementById("dashboardBtn");
+const assignmentsBtn = document.getElementById("assignmentsBtn");
+const tasksBtn = document.getElementById("tasksBtn");
+const scheduleBtn = document.getElementById("scheduleBtn");
+const logoutBtn = document.getElementById("logoutBtn");
--- login.js
+++ login.js
@@ -2,16 +2,22 @@ const userName = document.querySelector("#userName");
 const userPass = document.querySelector("#userPass");
 const loginBtn = document.querySelector("#loginBtn");
 const message = document.querySelector("#msg");
+console.log("login.js loaded");
+
+console.log(loginBtn);
 loginBtn.addEventListener("click", function(){
+    console.log("Login button clicked");
     let users=JSON.parse(localStorage.getItem("users")) || [];
     let foundUser=users.find(function(user){
         return user.username===userName.value;
-    })
+    });
+    console.log(loginBtn);
     if(foundUser){
         if(userPass.value===foundUser.password){
             message.textContent="Login Successful."
             message.style.color="green"
             localStorage.setItem("currentUser", foundUser.username);
+            console.log(localStorage.getItem("currentUser"));
             window.location.href="dashboard.html"
         }
         else
